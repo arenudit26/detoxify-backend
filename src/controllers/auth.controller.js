3ce7c13b428a7466ca6777ee54ed1db12f865067
arenudit26 <arenudit2626@gmail.com>
2025-12-08 23:50:55 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const googleCallback=async(req,res)=>{
 
         oauth2Client.setCredentials(tokens);
 
-        return res.redirect("http://localhost:5173");
+        return res.redirect("https://detoxifyproject-h0sxrgk34-udit-arens-projects.vercel.app/");
 
     }
      
