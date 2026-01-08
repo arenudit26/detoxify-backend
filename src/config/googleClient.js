fd324b42d2d273f85f93aa1285df035d87d66965
Udit Aren <arenudit2626@gmail.com>
2026-01-09 01:26:56 +0530

    disbale google oauth

--- src/config/googleClient.js
+++ src/config/googleClient.js
@@ -1,9 +1,9 @@
-import { google } from "googleapis";
+// import { google } from "googleapis";
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.OAUTH_REDIRECT_URI
-);
+// const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     process.env.OAUTH_REDIRECT_URI
+// );
 
-export default oauth2Client ;
+// export default oauth2Client ;
